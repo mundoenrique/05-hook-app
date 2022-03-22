3511275776fa8cc771072dce43dc345baebc015f
mundoenrique <mundoenrique@hotmail.com>
2022-03-21 22:34:37 -0400

    Pruebas sobre <HomeScreen />

---
+++ src/tests/09-UseContex/09-UseContex.test.js
@@ -0,0 +1,19 @@
+import { mount } from 'enzyme';
+import HomeScreen from '../../components/09-UseContex/HomeScreen';
+import { UserContext } from '../../components/09-UseContex/UserContext';
+
+describe('Pruebas sobre <HomeScreen />', () => {
+	const user = {
+		name: 'Enrique',
+		email: 'mundoenrique@gmail.com',
+	};
+	const wrapper = mount(
+		<UserContext.Provider value={{ user }}>
+			<HomeScreen />
+		</UserContext.Provider>
+	);
+
+	test('Debe renderizar <HomeScreen />', () => {
+		expect(wrapper).toMatchSnapshot();
+	});
+});
